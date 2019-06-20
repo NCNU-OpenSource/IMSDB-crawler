ff3acaf5cdfee25d41a1c4bc0c543527d3f19377
kent1201 <39613279+kent1201@users.noreply.github.com>
2019-06-20 15:15:04 +0800

    Add files via upload

---
+++ create.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+path="/home/kent1201/"
+filename="${path}""Dockershare/list.txt" 
+image="imsdb-crawler"
+count=10
+
+exec < ${filename}
+
+while IFS=' ' read -r url visited 
+do
+    echo "${url}"
+    #echo "${visited}"
+    if [ "${visited}" == "1" ]
+    then
+	echo "${url} is visited."
+    elif [ "${visited}" == "0" ]
+    then
+    	if [ "$i" -le "$count" ]
+	then
+		echo "Creating..."
+        	docker run --rm -dit -v "${path}"Dockershare:/Dockershare -e EXE="crul_script.py" -e URL="${url}"  ${image}
+        	OldLine="${url}"" ""${visited}"
+		#echo "OldLine = ${OldLine}"
+		visited="1"
+		NewLine="${url}"" ""${visited}"
+        	#echo "NewLine = ${NewLine}"
+		sed -e "s|${OldLine}|${NewLine}|g" -i ${filename}
+		i=$(expr $i + 1)
+	elif [ "$i" -eq "$count" ]
+	then
+		echo "I am sleeping."
+		sleep 120
+		i = 0
+	fi
+    else
+	exit 1
+    fi
+done
+exit 0
